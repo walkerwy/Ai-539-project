f0338a2f7e2a629a86027bd412c69239832eb0f5
unknown <walkerwy@oregonstate.edu>
2024-05-20 16:47:48 -0700

    made main, and other helper functions in order to be neat

--- train.py
+++ train.py
@@ -1,6 +1,56 @@
 # Wylea's Part
 
+#Source
+#https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
+#https://www.tensorflow.org/guide/basic_training_loops
+
+#what we talked about for the project:
+    #https://github.com/232525/PureT/tree/main
+    #https://github.com/berniwal/swin-transformer-pytorch
+
+
+
+#   From the hw 2 python parity file:
+
+# Basic python imports for logging and sequence generation
+import itertools
+import random
+import numpy as np
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+# Imports for Pytorch for the things we need
 import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Imports for progress bar
+from tqdm import tqdm
+
+# Imports for plotting our result curves
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("Agg")
+
+
+# Set random seed for python and torch to enable reproducibility (at least on the same hardware)
+random.seed(42)
+torch.manual_seed(42)
+
+# Determine if a GPU is available for use, define as global variable
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+#   From pytorch training documentation
+
+
+
 import torchvision
 import torchvision.transforms as transforms
 
@@ -8,23 +58,62 @@ import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
+def main():
+    # Swin transform goes here?
+    #transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))])
+    # Create datasets for training & validation, download if necessary
+    training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
+    validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
 
-# Create datasets for training & validation, download if necessary
-training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
-validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
+    # Create data loaders for our datasets; shuffle for training, not for validation
+    training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=4, shuffle=False)
 
-# Create data loaders for our datasets; shuffle for training, not for validation
-training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
-validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=4, shuffle=False)
+    # Class labels
+    #classes = (None)
 
-# Class labels
-classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+    #print(classes)
 
-# Report split sizes
-print('Training set has {} instances'.format(len(training_set)))
-print('Validation set has {} instances'.format(len(validation_set)))
+    # Report split sizes
+    print('Training set has {} instances'.format(len(training_set)))
+    print('Validation set has {} instances'.format(len(validation_set)))
+
+
+
+####################
+
+#Visualize progress:
+
+####################
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Helper function for inline image display
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def matplot_print(training_loader,validation_loader,classes):
+
+    dataiter = iter(training_loader)
+    images, labels = next(dataiter)
+
+    # Create a grid from the images and show them
+    img_grid = torchvision.utils.make_grid(images)
+    matplotlib_imshow(img_grid, one_channel=True)
+    print('  '.join(classes[labels[j]] for j in range(4)))
+
+
+
+if __name__ == "__main__":
+    main()
