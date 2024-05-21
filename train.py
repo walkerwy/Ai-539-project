404937d76e40917f88eb14ec9323474d1c05b3a6
unknown <walkerwy@oregonstate.edu>
2024-05-20 17:12:24 -0700

    cleaning up

--- train.py
+++ train.py
@@ -1,110 +1,74 @@
 # Wylea's Part
 
-#Source
-#https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
-#https://www.tensorflow.org/guide/basic_training_loops
+# Source
+# https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
+# https://www.tensorflow.org/guide/basic_training_loops
 
-#what we talked about for the project:
-    #https://github.com/232525/PureT/tree/main
-    #https://github.com/berniwal/swin-transformer-pytorch
+# Relevant project links:
+# https://github.com/232525/PureT/tree/main
+# https://github.com/berniwal/swin-transformer-pytorch
 
-
-
-#   From the hw 2 python parity file:
-
-# Basic python imports for logging and sequence generation
+# Imports
 import itertools
 import random
 import numpy as np
 import logging
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
-
-
-# Imports for Pytorch for the things we need
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
 import torch.nn as nn
 import torch.nn.functional as F
-
-# Imports for progress bar
 from tqdm import tqdm
-
-# Imports for plotting our result curves
-import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use("Agg")
-
-
-# Set random seed for python and torch to enable reproducibility (at least on the same hardware)
-random.seed(42)
-torch.manual_seed(42)
-
-# Determine if a GPU is available for use, define as global variable
-dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-
-#   From pytorch training documentation
-
-
-
 import torchvision
 import torchvision.transforms as transforms
-
-# PyTorch TensorBoard support
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
-def main():
-    # Swin transform goes here?
-    #transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
+# Set up logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-    # Create datasets for training & validation, download if necessary
-    training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
-    validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
+# Set random seed for reproducibility
+random.seed(42)
+torch.manual_seed(42)
 
-    # Create data loaders for our datasets; shuffle for training, not for validation
-    training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
-    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=4, shuffle=False)
+# Determine if a GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Class labels
-    #classes = (None)
+# FullTrainingDataset class
+class FullTrainingDataset(Dataset):
+    def __init__(self, full_ds, offset, length):
+        self.full_ds = full_ds
+        self.offset = offset
+        self.length = length
+        assert len(full_ds) >= offset + length, "Parent Dataset not long enough"
+        super(FullTrainingDataset, self).__init__()
 
-    #print(classes)
+    def __len__(self):
+        return self.length
 
-    # Report split sizes
-    print('Training set has {} instances'.format(len(training_set)))
-    print('Validation set has {} instances'.format(len(validation_set)))
+    def __getitem__(self, i):
+        return self.full_ds[i + self.offset]
 
-
-
-####################
-
-#Visualize progress:
-
-####################
-
-
-
-import matplotlib.pyplot as plt
-import numpy as np
+def trainTestSplit(dataset, val_share=0.2):
+    val_offset = int(len(dataset) * (1 - val_share))
+    return FullTrainingDataset(dataset, 0, val_offset), FullTrainingDataset(dataset, val_offset, len(dataset) - val_offset)
 
 # Helper function for inline image display
 def matplotlib_imshow(img, one_channel=False):
     if one_channel:
         img = img.mean(dim=0)
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
     else:
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
-def matplot_print(training_loader,validation_loader,classes):
-
+def matplot_print(training_loader, classes):
     dataiter = iter(training_loader)
     images, labels = next(dataiter)
 
@@ -113,7 +77,26 @@ def matplot_print(training_loader,validation_loader,classes):
     matplotlib_imshow(img_grid, one_channel=True)
     print('  '.join(classes[labels[j]] for j in range(4)))
 
+def main():
+    # Define transformations
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
+    # Create datasets for training & validation, download if necessary
+    training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
+    validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
+
+    # Create data loaders for our datasets; shuffle for training, not for validation
+    train_ds, val_ds = trainTestSplit(training_set)
+    train_loader = DataLoader(train_ds, batch_size=4, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=4, shuffle=False)
+
+    # Report split sizes
+    print(f'Training set has {len(training_set)} instances')
+    print(f'Validation set has {len(validation_set)} instances')
+
+    # Visualization
+    classes = [str(i) for i in range(10)]  # Example class labels for FashionMNIST
+    matplot_print(train_loader, classes)
 
 if __name__ == "__main__":
     main()
